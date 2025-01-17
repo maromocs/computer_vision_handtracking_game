345cd2289036d8f689fdd64871ed402e58a3a525
maromocs <marinosef@yahoo.com>
2025-01-17 14:55:37 +0200

    added feature that allows to drag and drop the square using "clicked method with index and middle finger"

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from cvzone.HandTrackingModule import HandDetector
 cap = cv2.VideoCapture(0)  # Use the default camera
 cap.set(3, 1280)  # Set width
 cap.set(4, 720)   # Set height
-colorR=(255,0,0)
+colorR=(255,0,0) #color used for rectangle (blue)
+
+cx,cy,tw,th=100,100,200,200
+
+
+#videocam check
 if not cap.isOpened():
     print("Error opening video stream check camera connection")
 
@@ -17,9 +22,9 @@ try:
             # draw a rectangle
             # Create an overlay for transparency
             overlay = img.copy()
-            cv2.rectangle(overlay, (100, 100), (300, 300), colorR, cv2.FILLED)
+            cv2.rectangle(overlay, (cx-tw//2, cy-th//2), (cx+th//2, cy+th//2), colorR, cv2.FILLED)
 
-            # Add transparency (alpha = 0.5 for 50% transparency)
+            # Add transparency (alpha = 0.3 for 70% transparency)
             alpha = 0.3
             cv2.addWeighted(overlay, alpha, img, 1 - alpha, 0, img)
             if not success:
@@ -57,9 +62,26 @@ try:
                     cv2.putText(img, handType, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
 
                     if adjusted_lmList:
+
+                        point1 = adjusted_lmList[8]
+                        point2 = adjusted_lmList[12]
+
+                        # Calculate the flipped positions for the line
+                        flipped_point1 = (1280 - point1[0], point1[1])  # Flip x-coordinate
+                        flipped_point2 = (1280 - point2[0], point2[1])  # Flip x-coordinate
+
+                        # find the length between of the 2 finger lm 8 and 2 from img and ignoring the rest of parameters
+                        l,_,_=detector.findDistance(flipped_point1,flipped_point2,img)
+                        print(l)
+
+
+
+
                         cursor=adjusted_lmList[8]
-                        if 100<cursor[0]<300 and 100<cursor[1]<300:
+                        #will work only if the distance of the index and middle fingers tips are close to each other
+                        if cx-tw//2<cursor[0]<cx+tw//2 and cy-th//2<cursor[1]<cy+th//2 and l<40:
                             colorR = (0, 255, 0)
+                            cx,cy=cursor #once the cursor is on the box cy and cx become the cursor
                         else:
                             colorR = (255, 0, 0)
 
